ec8571631e077f9c2488394fc7adc53011819874
aleks-karachevtsev <alekseyy.karachevcev@gmail.com>
2024-05-21 01:00:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
     var backgroundImage = document.querySelector('.hero');
-    backgroundImage.style.opacity = 1 - scrollPosition / 1000;
-    backgroundImage.style.transform = 'scale(' + (1 - scrollPosition / 2000) + ')';
-    if(scrollPosition > 100) {
+    var windowHeight = window.innerHeight;
+
+    if (scrollPosition < windowHeight) {
+        backgroundImage.style.opacity = 1 - scrollPosition / windowHeight;
+        if (window.innerWidth >= 769) {
+            backgroundImage.style.backgroundSize = (100 + scrollPosition / 10) + '%';
+        } else {
+            backgroundImage.style.backgroundSize = (150 + scrollPosition / 5) + '%';
+        }
+    }
+    
+    if (scrollPosition > 50) {
         backgroundImage.classList.add('animate');
     } else {
         backgroundImage.classList.remove('animate');
